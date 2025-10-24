cf1b9d25884e7a93f2adbce494a929eb132332a5
MJibranR <muhammadjibranrehan28@gmail.com>
2025-10-24 18:04:54 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
-import Image from "next/image";
+// import Header from '@/app/components/Header';
+import HeroSection from '@/app/components/HeroSection';
+import ServicesSection from '@/app/components/ServicesSection';
+import AboutSection from './components/AboutSection';
+import CTASection from '@/app/components/CTASection'
+// import Footer from '@/app/components/Footer';
 
 export default function Home() {
-  return <h1>HELLO WORLD</h1>;
+  return (
+    <main className="min-h-screen">
+      {/* <Header /> */}
+      <HeroSection />
+      <ServicesSection />
+      <AboutSection />
+      <CTASection />
+      {/* <Footer /> */}
+    </main>
+  );
 }
