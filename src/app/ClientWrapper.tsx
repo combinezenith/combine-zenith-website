1b800c2e504055adec7cba6b9ee2aa4809a0662e
Esha166 <eshacf4@gamil.com>
2025-10-26 00:30:13 +0500

    build error

---
+++ src/app/ClientWrapper.tsx
@@ -0,0 +1,24 @@
+import { usePathname } from 'next/navigation'
+import Header from "@/app/(components)/Header";
+import CursorSpark from "@/app/(components)/CursorSpark";
+import Footer from "@/app/(components)/Footer";
+import WhatsAppFloat from "@/app/(components)/WhatsAppFloat";
+import Agent from "@/app/(components)/Agent";
+export default function RootLayout({ children }: { children: React.ReactNode }) {  
+const pathname = usePathname()
+  const isAdmin = pathname.startsWith('/admin')
+  return (
+    <html lang="en">
+      <body
+        className='bg-background text-foreground'
+      >
+        {!isAdmin && <Header />}
+        {!isAdmin && <CursorSpark />}
+        {children}
+        {!isAdmin && <WhatsAppFloat />}
+        {!isAdmin && <Agent />}
+        {!isAdmin && <Footer />}
+      </body>
+    </html>
+  );
+}
