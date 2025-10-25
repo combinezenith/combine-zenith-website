dba53e8c7bec84d6f51ca63d323e7bbf504067de
MJibranR <muhammadjibranrehan28@gmail.com>
2025-10-25 21:46:33 +0500

    Blog page added

--- src/app/(pages)/blog/page.tsx
+++ src/app/(pages)/blog/page.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import FeaturedPost from "@/app/(components)/FeaturedPost";
+import BlogPosts from "@/app/(components)/BlogPosts";
 
-const Blog = () => {
+export default function Blog() {
   return (
-    <div>
-      <h1>This is Blog Page</h1>
+    <div className="bg-gradient-to-br from-indigo-950 via-purple-950 to-indigo-900">
+      <FeaturedPost />
+      <BlogPosts />
     </div>
   );
-};
-
-export default Blog;
+}
