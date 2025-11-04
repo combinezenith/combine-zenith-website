2d96bf2f105dcd8ddb71b2dfbfe2fad7a50487e8
MJibranR <muhammadjibranrehan28@gmail.com>
2025-11-05 00:28:30 +0500

    commit changes

--- src/app/(components)/HeroSection.tsx
+++ src/app/(components)/HeroSection.tsx
@@ -69,7 +69,7 @@ export default function HeroSection() {
 
       {/* Social Media Links */}
       <div aria-label="Social Media Links" className="absolute left-8 top-1/2 -translate-y-1/2 hidden xl:flex flex-col space-y-6 z-30">
-        <a href="#" aria-label="Facebook" className="text-white hover:text-purple-300 transition transform hover:scale-110 p-2 bg-white/10 backdrop-blur-sm rounded-full">
+        <a href="https://www.facebook.com/profile.php?id=61583245775368" aria-label="Facebook" className="text-white hover:text-purple-300 transition transform hover:scale-110 p-2 bg-white/10 backdrop-blur-sm rounded-full">
           <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
             <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z" />
           </svg>
