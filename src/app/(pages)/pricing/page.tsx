bd4704bd47b1a1006d74ead3e3eea73d194836f5
MJibranR <muhammadjibranrehan28@gmail.com>
2025-11-13 20:33:57 +0500

    committed

--- src/app/(pages)/pricing/page.tsx
+++ src/app/(pages)/pricing/page.tsx
@@ -2,12 +2,13 @@ import PricingPlan from '@/app/(components)/PricingPlan';
 import FeatureComparisonTable from '@/app/(components)/FeatureComparison';
 import PricCTA from '@/app/(components)/PricCTA';
 
-
 export default function Pricing() {
   return (
     <main className="min-h-screen">
       <PricingPlan />
-      <FeatureComparisonTable />
+      <div className="hidden md:block">
+        <FeatureComparisonTable />
+      </div>
       <PricCTA />      
     </main>
   );
