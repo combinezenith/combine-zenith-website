3fec1435f4b0cf0a6c98e920d80ef1b66eede1a3
MJibranR <muhammadjibranrehan28@gmail.com>
2025-11-19 18:17:07 +0500

    committed

--- src/app/(pages)/pricing/[id]/page.tsx
+++ src/app/(pages)/pricing/[id]/page.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import PricingDetail from "@/app/(components)/PricingDetail";
-import SubscriptionIncludes from "@/app/(components)/SubscriptionIncludes";
 import PlanCalculator from "@/app/(components)/PlanCalculator";
 
 interface PageProps {
@@ -17,7 +16,6 @@ export default async function PricingDetailPage({ params }: PageProps) {
   return (
     <div className="min-h-screen">
       <PricingDetail id={id} />
-      <SubscriptionIncludes />
       <div className="hidden md:block">
         <PlanCalculator />
       </div>
